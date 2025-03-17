6e1dd9a47196fabe0cdfa5ddc540aa4b801c2e6c
cjinhyeong <cjinhyeong@gmail.com>
2025-03-17 16:51:02 +0900

    new file:   task1.c new file:   task1.exe

---
+++ task1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    printf("2*1=%d\n", 2*1);
+    printf("2*2=%d\n", 2*2);
+    printf("2*3=%d\n", 2*3);
+    printf("2*4=%d\n", 2*4);
+    printf("2*5=%d\n", 2*5);
+    printf("2*6=%d\n", 2*6);
+    printf("2*7=%d\n", 2*7);
+    printf("2*8=%d\n", 2*8);
+    printf("2*9=%d\n", 2*9);
+    return 0;
+}
